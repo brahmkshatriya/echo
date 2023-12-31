9a9dfe27fe02ad71fc04e3b1d52e575eceff3b86
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2023-12-31 13:16:59 +0530

    Suppress Unstable Api in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
