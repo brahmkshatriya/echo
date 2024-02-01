5fb935e407ecb1a50893e04031a0c7afc23e7046
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-02-01 15:04:14 +0530

    Initial local extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,10 +46,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
-    implementation("androidx.media3:media3-exoplayer:1.2.0")
+    implementation("androidx.media3:media3-exoplayer:1.2.1")
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
@@ -59,6 +59,8 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
 
     implementation("io.coil-kt:coil:2.5.0")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.paging:paging-common-ktx:3.2.1")
 
     testImplementation(kotlin("reflect"))
     testImplementation("junit:junit:4.13.2")
