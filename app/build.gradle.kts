6982ec896d9780464ac2b6d70ca2a5c58b439ca7
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-02-05 17:38:30 +0530

    Update Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0-alpha03")
 
     implementation("com.google.dagger:hilt-android:2.48.1")
     implementation("androidx.media3:media3-session:1.2.1")
