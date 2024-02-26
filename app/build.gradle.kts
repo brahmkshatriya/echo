b20c328e59e2885eb4fd13e9b96a36fd54ad1cd3
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-02-26 18:00:55 +0530

    rollback to hilt 2.48, because 2.50 is fucked

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,8 +68,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.8.0")
 
     implementation("com.google.android.material:material:1.12.0-alpha03")
-    implementation("com.google.dagger:hilt-android:2.50")
-    ksp("com.google.dagger:hilt-android-compiler:2.50")
+
+    //noinspection GradleDependency
+    implementation("com.google.dagger:hilt-android:2.48")
+    //noinspection GradleDependency
+    ksp("com.google.dagger:hilt-android-compiler:2.48")
 
     implementation("io.coil-kt:coil:2.6.0")
 
