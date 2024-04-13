69a9ab3f1a1f3b6368210cfb33071122ca6d232d
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-04-13 14:18:40 +0530

    [skip ci] remove test extension

--- app/src/main/java/dev/brahmkshatriya/echo/data/utils/LocalExtensionRepo.kt
+++ app/src/main/java/dev/brahmkshatriya/echo/data/utils/LocalExtensionRepo.kt
@@ -11,7 +11,6 @@ class LocalExtensionRepo(val context: Context) : PluginRepo<ExtensionClient> {
         flowOf(
             listOf(
                 OfflineExtension(context),
-//                TestExtension()
             )
         )
 }
