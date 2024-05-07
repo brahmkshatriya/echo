4d98be281bc09be4af7ff876bed1615f0177210b
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-05-07 12:38:17 +0530

    [skip ci] causes of sleep deprivation

--- common/src/main/java/dev/brahmkshatriya/echo/common/models/Lyrics.kt
+++ common/src/main/java/dev/brahmkshatriya/echo/common/models/Lyrics.kt
@@ -4,7 +4,7 @@ data class Lyrics(
     val id: String,
     val title: String,
     val subtitle: String? = null,
-    val lyrics: List<String>? = null,
+    val lyrics: List<Lyric>? = null,
     val fillTimeGaps : Boolean = true,
     val extras: Map<String, String> = emptyMap()
 )
