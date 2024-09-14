4e9867db1a263f0aca8184cc3bc56d8c83e7cf09
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2024-09-14 14:11:30 +0200

    Small refactor

--- app/src/main/java/dev/brahmkshatriya/echo/playback/ByteChannelDataSource.kt
+++ app/src/main/java/dev/brahmkshatriya/echo/playback/ByteChannelDataSource.kt
@@ -21,10 +21,9 @@ class ByteChannelDataSource : BaseDataSource(true) {
     }
 
     private var audio: Streamable.Audio.Channel? = null
-    private var channel: ByteReadChannel? = null
 
     override fun read(buffer: ByteArray, offset: Int, length: Int): Int {
-        val channel = channel ?: throw IOException("Channel is not open")
+        val channel = audio!!.channel
         return runBlocking {
             val bytesRead = channel.readAvailable(buffer, offset, length)
             if (bytesRead == -1) C.RESULT_END_OF_INPUT else bytesRead
@@ -34,39 +33,26 @@ class ByteChannelDataSource : BaseDataSource(true) {
     override fun open(dataSpec: DataSpec): Long {
         val audio = dataSpec.customData as Streamable.Audio.Channel
         val requestedPosition = dataSpec.position
-
-        channel = audio.channel
-        this.audio = audio
-
         runBlocking {
-            seekChannelToPosition(channel!!, requestedPosition)
+            audio.channel.seek(requestedPosition)
         }
-
-        return audio.totalBytes - requestedPosition
+        this.audio = audio
+        return audio.totalBytes
     }
 
     override fun getUri() = audio?.hashCode().toString().toUri()
 
     override fun close() {
         runBlocking {
-            channel?.cancel()
+            audio?.channel?.cancel()
         }
-        channel = null
         audio = null
     }
 
-    private suspend fun seekChannelToPosition(channel: ByteReadChannel, requestedPosition: Long) {
-        if (requestedPosition > 0) {
-            var remaining = requestedPosition
-            val discardBuffer = ByteArray(8192)
-            while (remaining > 0) {
-                val toRead = min(remaining, discardBuffer.size.toLong()).toInt()
-                val readBytes = channel.readAvailable(discardBuffer, 0, toRead)
-                if (readBytes == -1) {
-                    throw IOException("Reached end of stream before desired position")
-                }
-                remaining -= readBytes
-            }
+    private suspend fun ByteReadChannel.seek(requestedPosition: Long) {
+        val discarded = discard(requestedPosition)
+        if (discarded < requestedPosition) {
+            throw IOException("Reached end of stream before desired position")
         }
     }
 }
