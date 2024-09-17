f2a7f86fbc5aabb83a31ea42b084e3b16baab6e1
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2024-09-17 12:55:28 +0530

    Fix not set

--- app/src/main/java/dev/brahmkshatriya/echo/utils/prefs/MaterialTextInputPreference.kt
+++ app/src/main/java/dev/brahmkshatriya/echo/utils/prefs/MaterialTextInputPreference.kt
@@ -49,7 +49,7 @@ class MaterialTextInputPreference(context: Context) : EditTextPreference(context
 
     private fun updateSummary() {
         val value = context.getString(R.string.value)
-        val entry = text ?: context.getString(R.string.not_set)
+        val entry = text ?: context.getString(R.string.str_not_set)
         val sum = customSummary?.let { "\n\n$it" } ?: ""
         summary = "$value : $entry$sum"
     }
