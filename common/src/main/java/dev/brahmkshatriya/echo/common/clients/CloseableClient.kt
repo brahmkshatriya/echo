fedf229e9f73a3e24745465cbe9feb08acf8a86e
rebel onion <87634197+rebelonion@users.noreply.github.com>
2024-11-23 02:32:59 -0600

    feat: docs (crazy, I know)

--- common/src/main/java/dev/brahmkshatriya/echo/common/clients/CloseableClient.kt
+++ common/src/main/java/dev/brahmkshatriya/echo/common/clients/CloseableClient.kt
@@ -1,5 +1,9 @@
 package dev.brahmkshatriya.echo.common.clients
 
 interface CloseableClient {
+    /**
+     * Called when the app and player are closed.
+     * Useful for cleaning up resources.
+     */
     fun close()
 }
--- common/src/main/java/dev/brahmkshatriya/echo/common/clients/MessagePostClient.kt
+++ common/src/main/java/dev/brahmkshatriya/echo/common/clients/MessagePostClient.kt
@@ -1,6 +1,17 @@
 package dev.brahmkshatriya.echo.common.clients
 
 interface MessagePostClient {
+    /**
+     * Posts a message to the user.
+     * Call this when you want to display a notification to the user.
+     *
+     * @param message The message text to display
+     */
     fun postMessage(message: String)
+
+    /**
+     * Internal setup method used by the main app.
+     * Extensions should not call this method.
+     */
     fun setMessageHandler(handler: (String) -> Unit)
 }
