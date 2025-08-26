f69dd703aa64e38377058a7d93f2eaae795287f5
Shivam <69040506+brahmkshatriya@users.noreply.github.com>
2025-08-26 08:31:52 +0000

    [skip ci] better dokka

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlinx.serialization)
     id("maven-publish")
-    id("org.jetbrains.dokka") version "1.9.10"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 java {
@@ -30,5 +30,22 @@ publishing {
 
             from(components["java"])
         }
+    }
+}
+
+dokka {
+    moduleName.set("common")
+    moduleVersion.set("1.0")
+    dokkaSourceSets.main {
+        includes.from("README.md")
+        sourceLink {
+            localDirectory.set(file("src/main/java"))
+            remoteUrl("https://github.com/brahmkshatriya/echo/tree/main/common/src/main/java")
+            remoteLineSuffix.set("#L")
+        }
+    }
+    pluginsConfiguration.html {
+        customStyleSheets.from("styles.css")
+        footerMessage.set("made by <a style=\"color: inherit; text-decoration: underline;\" href=\"https://github.com/brahmkshatriya\">@brahmkshatriya</a>")
     }
 }
