12545735a5471944e83dc7cb2a684f9b28ad399d
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2025-09-01 12:47:11 +0530

    fix crash on 3 dots

--- app/src/main/java/dev/brahmkshatriya/echo/extensions/cache/Cached.kt
+++ app/src/main/java/dev/brahmkshatriya/echo/extensions/cache/Cached.kt
@@ -258,14 +258,8 @@ object Cached {
             PagedData.Continuous { token ->
                 val id = "$id-$token"
                 val page = fileCache.getData<Page<T>>(id).getOrThrow()
-                page.copy(page.data.map { transform(it) }).also {
-                    println("Got page $id with ${it.data.size} items" )
-                }
-            }.toFeedData(buttons, bg).also {
-                println("Got feed data $id with $it" )
-            }
-        }.also {
-            println("Got $tabId from cache with ${tabs.size} tabs")
+                page.copy(page.data.map { transform(it) })
+            }.toFeedData(buttons, bg)
         }
     }
 
